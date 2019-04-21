a9449285fc9d744ca98c70991a4524f2f9918833
helentsai <pupu.her@hotmail.com>
2019-04-21 17:19:40 -0400

    Initial commit

---
+++ app/src/main/java/tte/com/funbackgroung/MainActivity.java
@@ -0,0 +1,40 @@
+package tte.com.funbackgroung;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout layout;
+    private ImageButton button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+        button = findViewById(R.id.imageButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+
+                int color = Color.argb(255,random.nextInt(256),random.nextInt(256),random.nextInt(256));
+
+                layout.setBackgroundColor(color);
+
+            }
+        });
+
+
+    }
+}
